c0e598436eef1d7d75a8cce9bf5736ae68953d27
Randy Calderon <randycweb@gmail.com>
2019-02-11 15:31:49 -0800

    Added country and countrylist classes

--- countries/src/main/java/com/lambdaschool/countries/Country.java
+++ countries/src/main/java/com/lambdaschool/countries/Country.java
@@ -1,5 +1,56 @@
 package com.lambdaschool.countries;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Country {
-    
+    private static final AtomicLong counter = new AtomicLong();
+    private long id;
+    private String countryName;
+    private int countryAge;
+    private int landingMassSize;
+    private int population;
+
+    public Country(String countryName, int countryAge, int landingMassSize, int population) {
+        this.id = counter.incrementAndGet();
+        this.countryName = countryName;
+        this.countryAge = countryAge;
+        this.population = population;
+        this.landingMassSize = landingMassSize;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCountryAge() {
+        return countryAge;
+    }
+
+    public void setCountryAge(int countryAge) {
+        this.countryAge = countryAge;
+    }
+
+    public int getLandingMassSize() {
+        return landingMassSize;
+    }
+
+    public void setLandingMassSize(int landingMassSize) {
+        this.landingMassSize = landingMassSize;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 }
